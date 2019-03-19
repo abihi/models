e6cf183912ab6e625508a42a279701854f09c411
abihi <ahmed.bihi@hotmail.com>
2019-03-19 10:34:50 +0100

    conversion to tflite

--- models/research/deeplab/convert_to_tflite_quant.py
+++ models/research/deeplab/convert_to_tflite_quant.py
@@ -7,7 +7,7 @@ shapes = {
   "ImageTensor": [1,513,513,3]
 }
 
-converter = tf.lite.TFLiteConverter.from_frozen_graph(
+converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(
   graph_def_file, input_arrays, output_arrays, input_shapes=shapes)
 tflite_model = converter.convert()
 open("mobilenetv2_pascalVOC.tflite", "wb").write(tflite_model)
